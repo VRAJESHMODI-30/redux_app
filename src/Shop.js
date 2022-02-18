228d7732d0cbda7f60d5d0b2c77039627bfe73d5
Vrajesh Modi <91773413+VRAJESHMODI-30@users.noreply.github.com>
2022-02-18 18:02:38 +0530

    Setting up the redux_app

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div>
+      <h5>Apple iPhone 13 (128 GB Storage, Midnight)</h5>
+      <button className="btn btn-outline-primary mx-2">-</button>
+      Add to cart
+      <button className="btn btn-outline-primary mx-2">+</button>
+    </div>
+  );
+};
+
+export default Shop;
